38b7582dbda090f20f92809fa0e45253ec51f062
Robert Berg <rberg@cps.edu>
2017-05-31 18:28:50 +0000

    2017/2018 classes added to main page

---
+++ iyu/StateTournament/script/teams.js
@@ -0,0 +1,543 @@
+
+var teams =
+[
+    {
+        "teamid": 1,
+        "name": "Reavis Rams",
+        "division": "HSB",
+        "seed": 2,
+        "pool": "B",
+        "poolseeding": "B1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 2,
+        "name": "Fox Valley Fusion A",
+        "division": "HSA",
+        "seed": 4,
+        "pool": "D",
+        "poolseeding": "D1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 3,
+        "name": "Fox Valley Fusion B",
+        "division": "HSB",
+        "seed": 1,
+        "pool": "A",
+        "poolseeding": "A1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 4,
+        "name": "Fox Valley Fusion A",
+        "division": "MSA",
+        "seed": 2,
+        "pool": "A",
+        "poolseeding": "A2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 5,
+        "name": " Fox Valley Fusion ",
+        "division": "MSB",
+        "seed": 2,
+        "pool": "A",
+        "poolseeding": "A2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 6,
+        "name": "Colossus of Latin",
+        "division": "HSB",
+        "seed": 3,
+        "pool": "C",
+        "poolseeding": "C1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 7,
+        "name": "Prtizker Jaguars",
+        "division": "HSA",
+        "seed": 12,
+        "pool": "A",
+        "poolseeding": "A3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 8,
+        "name": "Pritzker Lady Jaguars",
+        "division": "HSG",
+        "seed": 5,
+        "pool": "A",
+        "poolseeding": "A5",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 9,
+        "name": "Walter Payton Grizzlies",
+        "division": "HSG",
+        "seed": 3,
+        "pool": "A",
+        "poolseeding": "A3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 10,
+        "name": "Northstar Ultimate",
+        "division": "HSA",
+        "seed": 10,
+        "pool": "C",
+        "poolseeding": "C3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 11,
+        "name": "Northstar Ultimate JV",
+        "division": "HSB",
+        "seed": 10,
+        "pool": "C",
+        "poolseeding": "C3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 12,
+        "name": "Haines Hurricane Ultimate",
+        "division": "MSB",
+        "seed": 1,
+        "pool": "A",
+        "poolseeding": "A1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 13,
+        "name": "NV Ultimate A",
+        "division": "HSA",
+        "seed": 1,
+        "pool": "A",
+        "poolseeding": "A1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 14,
+        "name": "NV Ultimate B",
+        "division": "HSA",
+        "seed": 7,
+        "pool": "B",
+        "poolseeding": "B2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 15,
+        "name": "NV Ultimate Bravo",
+        "division": "HSB",
+        "seed": 5,
+        "pool": "D",
+        "poolseeding": "D2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 16,
+        "name": "NV Ultimate Blue",
+        "division": "HSB",
+        "seed": 6,
+        "pool": "C",
+        "poolseeding": "C2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 17,
+        "name": "NV Ultimate Gold",
+        "division": "HSB",
+        "seed": 8,
+        "pool": "A",
+        "poolseeding": "A2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 18,
+        "name": "NV Youth A",
+        "division": "MSA",
+        "seed": 1,
+        "pool": "A",
+        "poolseeding": "A1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 19,
+        "name": "NV Youth X",
+        "division": "MSB",
+        "seed": 3,
+        "pool": "A",
+        "poolseeding": "A3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 20,
+        "name": "NV Youth Y",
+        "division": "MSB",
+        "seed": 4,
+        "pool": "A",
+        "poolseeding": "A4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 21,
+        "name": "HP Giants Ultimate",
+        "division": "HSA",
+        "seed": 17,
+        "pool": "A",
+        "poolseeding": "A5",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 22,
+        "name": "Evanston Wildkits",
+        "division": "HSA",
+        "seed": 11,
+        "pool": "B",
+        "poolseeding": "B3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 23,
+        "name": "eNVy X",
+        "division": "HSG",
+        "seed": 1,
+        "pool": "A",
+        "poolseeding": "A1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 24,
+        "name": "eNVy Y",
+        "division": "HSG",
+        "seed": 2,
+        "pool": "A",
+        "poolseeding": "A2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 25,
+        "name": "Saint Charles East",
+        "division": "HSA",
+        "seed": 13,
+        "pool": "A",
+        "poolseeding": "A4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 26,
+        "name": "Geneva Vikings",
+        "division": "HSA",
+        "seed": 2,
+        "pool": "B",
+        "poolseeding": "B1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 27,
+        "name": "Geneva Vikings Girls",
+        "division": "HSG",
+        "seed": 4,
+        "pool": "A",
+        "poolseeding": "A4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 28,
+        "name": "Geneva Vikings JV",
+        "division": "HSB",
+        "seed": 9,
+        "pool": "D",
+        "poolseeding": "D3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 29,
+        "name": "Geneva Vikings FS",
+        "division": "HSB",
+        "seed": 13,
+        "pool": "A",
+        "poolseeding": "A4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 30,
+        "name": "Geneva Vikings A",
+        "division": "MSA",
+        "seed": 3,
+        "pool": "A",
+        "poolseeding": "A3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 31,
+        "name": "Geneva Vikings B",
+        "division": "MSB",
+        "seed": 5,
+        "pool": "A",
+        "poolseeding": "A5",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 32,
+        "name": "Downers Grove South",
+        "division": "HSB",
+        "seed": 4,
+        "pool": "D",
+        "poolseeding": "D1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 33,
+        "name": "E'ville Elite ",
+        "division": "HSA",
+        "seed": 9,
+        "pool": "D",
+        "poolseeding": "D3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 34,
+        "name": "Naperville Central",
+        "division": "HSA",
+        "seed": 3,
+        "pool": "C",
+        "poolseeding": "C1",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 35,
+        "name": "OPRF Revolution",
+        "division": "HSA",
+        "seed": 5,
+        "pool": "D",
+        "poolseeding": "D2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 36,
+        "name": "OPRF Revolution Blue",
+        "division": "HSB",
+        "seed": 7,
+        "pool": "B",
+        "poolseeding": "B2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 37,
+        "name": "Not Trier Ultimate",
+        "division": "HSA",
+        "seed": 19,
+        "pool": "C",
+        "poolseeding": "C5",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 38,
+        "name": "Gatortangs",
+        "division": "HSA",
+        "seed": 8,
+        "pool": "A",
+        "poolseeding": "A2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 39,
+        "name": "Gatortangs- JV",
+        "division": "HSB",
+        "seed": 12,
+        "pool": "A",
+        "poolseeding": "A3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 40,
+        "name": "Lane Tech",
+        "division": "HSA",
+        "seed": 18,
+        "pool": "B",
+        "poolseeding": "B5",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 41,
+        "name": "Naperville North",
+        "division": "HSA",
+        "seed": 16,
+        "pool": "D",
+        "poolseeding": "D4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 42,
+        "name": "Julian Jayhawks",
+        "division": "MSA",
+        "seed": 4,
+        "pool": "A",
+        "poolseeding": "A4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 43,
+        "name": "Metea Valley Mustangs",
+        "division": "HSA",
+        "seed": 15,
+        "pool": "C",
+        "poolseeding": "C4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 44,
+        "name": "Metea Valley Mustangs JV1",
+        "division": "HSB",
+        "seed": 11,
+        "pool": "B",
+        "poolseeding": "B3",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 45,
+        "name": "Metea Valley Mustangs JV2",
+        "division": "HSB",
+        "seed": 14,
+        "pool": "B",
+        "poolseeding": "B4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 46,
+        "name": "Plainfield North",
+        "division": "HSA",
+        "seed": 6,
+        "pool": "C",
+        "poolseeding": "C2",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 47,
+        "name": "Elmhurst Evolution",
+        "division": "HSA",
+        "seed": 14,
+        "pool": "B",
+        "poolseeding": "B4",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 48,
+        "name": "Lake Bluff MS",
+        "division": "MSA",
+        "seed": 5,
+        "pool": "A",
+        "poolseeding": "A5",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    },
+    {
+        "teamid": 49,
+        "name": "Lincoln-Way East Mirror Image",
+        "division": "HSA",
+        "seed": 20,
+        "pool": "D",
+        "poolseeding": "D5",
+        "poolplaywins": 0,
+        "poolplaylosses": 0
+    }
+];
+
+var countHSA = 0;
+var countHSB = 0;
+var countHSG = 0;
+var countMSA = 0;
+var countMSB = 0;
+for(var i=0; i<teams.length;i++){
+    if(teams[i].division == "HSA"){
+        countHSA++;
+    } else if(teams[i].division == "HSB"){
+        countHSB++;
+    } else if(teams[i].division == "HSG"){
+        countHSG++;
+    } else if(teams[i].division == "MSA"){
+        countMSA++;
+    } else{
+        countMSB++;
+    }
+}
+
+var teamsHSA = new Array(countHSA);
+var teamsHSB = new Array(countHSB);
+var teamsHSG = new Array(countHSG);
+var teamsMSA = new Array(countMSA);
+var teamsMSB = new Array(countMSB);
+
+var placeHSA = 0, placeHSB = 0, placeHSG = 0, placeMSA = 0, placeMSB = 0;
+
+for(var i=0; i<teams.length;i++){
+    if(teams[i].division == "HSA"){
+        teamsHSA[placeHSA] = teams[i];
+        placeHSA++;
+    } else if(teams[i].division == "HSB"){
+        teamsHSB[placeHSB] = teams[i];
+        placeHSB++;
+    } else if(teams[i].division == "HSG"){
+        teamsHSG[placeHSG] = teams[i];
+        placeHSG++;
+    } else if(teams[i].division == "MSA"){
+        teamsMSA[placeMSA] = teams[i];
+        placeMSA++;
+    } else{
+        teamsMSB[placeMSB] = teams[i];
+        placeMSB++;
+    }
+}
+
+
+
